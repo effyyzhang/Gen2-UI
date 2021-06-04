bd142538aba785f443aded306938f9a10ed42e0d
Effy Zhang <effyyzhang@gmail.com>
2021-06-03 21:47:02 -0700

    updated font and the card shape

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2171454e6b60cc40a118"; }
+/******/ 		__webpack_require__.h = function() { return "139ecc8ab472699ec3c5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
